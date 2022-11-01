96fdd032ebb209c3af1ea889cd1988f3a2ae2fbe
William Gadd <wgadd@sasktel.net>
2022-11-01 14:41:49 -0600

    Did some cleanup and simplified conditional for a full item

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,9 +4,18 @@ import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
   const full = (props.spots === 0);
-  const itemClass = classNames("day-list__item", {"day-list__item--selected": props.selected, "day-list__item--full": full})
+  const itemClass = classNames(
+    "day-list__item", 
+    {
+      "day-list__item--selected": props.selected, 
+      "day-list__item--full": !props.spots,
+    }
+  );
   return (
-    <li className={itemClass} onClick={() => props.setDay(props.name)}>
+    <li 
+      className={itemClass} 
+      onClick={() => props.setDay(props.name)}
+    >
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{`${props.spots} spots remaining`}</h3>
     </li>
