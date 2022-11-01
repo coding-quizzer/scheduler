b62106149bd45462663e027e29b739bde86e047d
William Gadd <wgadd@sasktel.net>
2022-11-01 11:27:40 -0600

    Apply button styles to buttons and apply styles for confirm and danger props

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,8 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
-}
+  let buttonClass = "button";
+  buttonClass += props.confirm ? " button--confirm" : "";
+  buttonClass += props.danger ? " button--danger" : "";
+  return <button className={buttonClass}>{props.children}</button>;
+};
