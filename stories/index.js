c93d5d3e31876ca6386f61e69fca3a6a5ef1b660
William Gadd <wgadd@sasktel.net>
2022-11-02 15:08:57 -0600

    Fix typo in avatar url of the thrd interviewer for the IntervewerList component

--- stories/index.js
+++ stories/index.js
@@ -109,7 +109,7 @@ storiesOf("Button", module)
         { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
         { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
         { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-        { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FKBV841.jpg" },
+        { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
         { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
       ];
 
@@ -128,7 +128,7 @@ storiesOf("Button", module)
             interviewer={3}
           />
         ))
-        .add("CLickable", () => (
+        .add("Clickable", () => (
           <InterviewerList
             interviewers={interviewers}
             setInterviewer={action("setInterviewer")}
