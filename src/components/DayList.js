6249c01d1048b4ea9b08a75d3442c4c2d79c5885
William Gadd <wgadd@sasktel.net>
2022-11-01 18:05:32 -0600

    Generate list of DayListItems in DayList component

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,9 +1,19 @@
 import React from "react";
 import DayListItem from "./DayListItem";
 
-const DayList = () => {
+const DayList = (props) => {
   return (
-    <ul></ul>
+    <ul>
+      {props.days.map((day) => (
+        <DayListItem
+          key={day.id}
+          name={day.name}
+          spots={day.spots}
+          selected={day.name === props.day}
+          setDay={props.setDay}
+        />
+      ))}
+    </ul>
   )
 }
 
