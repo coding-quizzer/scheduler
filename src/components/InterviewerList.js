4bd10f2372939c336a2c8cb0c9c51f464ff6fce1
William Gadd <wgadd@sasktel.net>
2022-11-02 15:10:52 -0600

    Use interviewers.map to display all the intervewer list items in the interviewer list

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,13 +1,29 @@
 import React from "react";
+import classNames from "classnames";
 import InterviewerListItem from "./InterviewerListItem";
 
 import "components/InterviewerList.scss"
 
 const InterviewerList = (props) => {
+  const { interviewers, setInterviewer, interviewer } = props;
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">
+        {interviewers.map((currentInterviewer) => {
+          const{ id, name, avatar } = currentInterviewer;
+          return (
+            <InterviewerListItem
+              key={id}
+              id={id}
+              name={name}
+              avatar={avatar}
+              selected={interviewer===id}
+              setInterviewer={() => setInterviewer(id)}
+            />
+          )
+          })}
+      </ul>
     </section>
   )
 
