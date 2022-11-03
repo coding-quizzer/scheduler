e8c304efc67227d0fc1070c98b90ca9d41248851
William Gadd <wgadd@sasktel.net>
2022-11-02 22:34:10 -0600

    Add Appointment data with hardcoded data into the main application

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import DayList from "./DayList";
+import Appointment from "./Appointment";
 import "components/Application.scss";
 
 
@@ -23,6 +24,55 @@ export default function Application(props) {
       spots: 0,
     },
    ];
+
+   const appointments = {
+    "1": {
+      id: 1,
+      time: "12pm",
+    },
+    "2": {
+      id: 2,
+      time: "1pm",
+      interview: {
+        student: "Lydia Miller-Jones",
+        interviewer:{
+          id: 3,
+          name: "Sylvia Palmer",
+          avatar: "https://i.imgur.com/LpaY82x.png",
+        }
+      }
+    },
+    "3": {
+      id: 3,
+      time: "2pm",
+    },
+    "4": {
+      id: 4,
+      time: "3pm",
+      interview: {
+        student: "Archie Andrews",
+        interviewer:{
+          id: 4,
+          name: "Cohana Roy",
+          avatar: "https://i.imgur.com/FK8V841.jpg",
+        }
+      }
+    },
+    "5": {
+      id: 5,
+      time: "4pm",
+    }
+  };
+
+  const appointmentList = Object.values(appointments);
+
+  const appointmentBody = appointmentList.map(appointment => (
+    <Appointment
+      key={appointment.id}
+      {...appointment}
+    />
+  ))
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -46,7 +96,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentBody}
       </section>
     </main>
   );
