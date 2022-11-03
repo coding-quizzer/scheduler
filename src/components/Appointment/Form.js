980293d5a25c6793dfd378dcd5fa7754e7893ad3
William Gadd <wgadd@sasktel.net>
2022-11-02 20:49:17 -0600

    Create Form Component with hard-coded values and no states yet

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+import InterviewerList from "components/InterviewerList";
+import Button from "components/Button";
+
+const interviewers = [
+  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+];
+
+
+const Form = (props) => {
+
+  
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            /*
+            This must be a controlled component
+            your code goes here
+            
+            */
+          />
+        </form>
+        <InterviewerList
+          interviewers={interviewers}
+          interviewer={1}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger>Cancel</Button>
+          <Button confirm>Save</Button>
+        </section>
+      </section>
+    </main>
+  )
+}
