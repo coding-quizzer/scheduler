5bac7958e1f5c8e360ecb8e27aef38191c07b95f
William Gadd <wgadd@sasktel.net>
2022-11-02 21:41:16 -0600

    Make it so that cancel button clears the form and calls the onCancel prop

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -9,6 +9,14 @@ const Form = (props) => {
   const {interviewers,  onSave, onCancel} = props
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.student || null);
+  const reset = () => {
+    setInterviewer(null);
+    setStudent ("");
+  }
+  const cancel = () => {
+    reset();
+    onCancel();
+  }
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -35,7 +43,7 @@ const Form = (props) => {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={onCancel}>Cancel</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
           <Button confirm onClick={onSave}>Save</Button>
         </section>
       </section>
