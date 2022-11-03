f9df9ae669733b10d9820c1f77e0e15fd51fb6a3
William Gadd <wgadd@sasktel.net>
2022-11-02 21:04:08 -0600

    Add stories for create form and edit form

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 
 storiesOf("Button", module)
@@ -174,4 +175,21 @@ storiesOf("Appointment", module)
       message="Could not delete appointment."
       onClose={action('onClose')}
     />
+  ))
+  .add("Form create", () => (
+    <Form
+      interviewers={interviewers}
+      onSave={action('onSave')}
+      onCancel={action('onCancel')}
+    />
+  ))
+  .add("Form edit", () => (
+    <Form
+      student="Lydia Miller-Jones"
+      interviewers={interviewers}
+      interviewer={3}
+      onSave={action('onSave')}
+      onCancel={action('onCancel')}
+    />
+
   ));
