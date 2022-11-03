dcd8324e6dc2595793dca1fba0df477afa9faaf1
William Gadd <wgadd@sasktel.net>
2022-11-03 17:33:36 -0600

    Use axios to fetch information for the days header and display it on the sadebar/header

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import "components/Application.scss";
@@ -7,23 +7,12 @@ import "components/Application.scss";
 
 export default function Application(props) {
   const [day, setDay] = useState('Monday');
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-   ];
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/days')
+      .then((response) => setDays(response.data))
+  }, [])
 
    const appointments = {
     "1": {
