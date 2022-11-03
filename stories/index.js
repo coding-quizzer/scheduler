ac1cf4747d2989fc62d9af77217e41b3066d68c6
William Gadd <wgadd@sasktel.net>
2022-11-02 22:24:22 -0600

    Add Appointment story for when there is an appointment booked

--- stories/index.js
+++ stories/index.js
@@ -199,4 +199,14 @@ storiesOf("Appointment", module)
       <Appointment time="5pm" />
     </Fragment>
     
+  ))
+  .add("Appointment Booked", () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="4pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }}
+        />
+      <Appointment time="5pm" />
+    </Fragment>
   ));
