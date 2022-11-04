530d891e78d51bf2e5ea8686e194996060560477
William Gadd <wgadd@sasktel.net>
2022-11-04 12:22:29 -0600

    Set interviewer state after fetching data and use helper function to send the full object with interviewer details to the Appointment component as a prop

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
+
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import "components/Application.scss";
-import { getAppointmentsForDay } from "helpers/selectors";
+
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 
 export default function Application(props) {
@@ -25,8 +27,7 @@ export default function Application(props) {
       axios.get('/api/interviewers')
     ])
     .then(all => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
-      console.log(all[2].data);
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
     });
   }, []);
 
