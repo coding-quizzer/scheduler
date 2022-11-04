5be00cff1d3e365b9346af469b3732a8fee743de
William Gadd <wgadd@sasktel.net>
2022-11-04 11:22:25 -0600

    Add interviewers to data retrieved by axios and print the data to the console

--- src/components/Application.js
+++ src/components/Application.js
@@ -21,10 +21,12 @@ export default function Application(props) {
   useEffect(() => {
     Promise.all([
       axios.get('/api/days'),
-      axios.get('/api/appointments')
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
     ])
     .then(all => {
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
+      console.log(all[2].data);
     });
   }, []);
 
