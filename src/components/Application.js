8460743f88d6f1934b434a5e04ffecd80df97454
William Gadd <wgadd@sasktel.net>
2022-11-03 20:09:43 -0600

    Set state with values returned from Promise.all

--- src/components/Application.js
+++ src/components/Application.js
@@ -23,8 +23,7 @@ export default function Application(props) {
       axios.get('/api/appointments')
     ])
     .then(all => {
-      console.log(all[0].data);
-      console.log(all[1].data)
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
     });
   }, [])
 
