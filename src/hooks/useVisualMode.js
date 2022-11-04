58eee8c6db28969c70a4763660db31578163e676
William Gadd <wgadd@sasktel.net>
2022-11-04 16:13:01 -0600

    Add test for back function that reverts to the previous state. Impliment the back function for useVisualMode, using history as the new state"

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,7 +1,22 @@
 import { useState } from "react";
 export default function useVisualMode(initMode) {
-  const [mode, setMode] = useState(initMode);
+  const [history, setHistory] = useState([initMode]);
 
-  return { mode, transition: setMode };
+  const transition = (newMode) => {
+    setHistory(prev => ([...prev, newMode]));
+  }
+
+  const mode = history[history.length - 1];
+
+
+
+  const back = () => {
+    const newHistory = history.slice(0, -1);
+    console.log(history)
+    setHistory(newHistory);
+
+  };
+
+  return { mode, transition, back };
 
 };
