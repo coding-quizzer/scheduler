539a600a6b787c999f57b7f27d20939d96df0585
William Gadd <wgadd@sasktel.net>
2022-11-04 17:19:18 -0600

    Use UseVisualMode hook for the Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,16 +1,33 @@
 import React from "react";
+
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+
+import useVisualMode from "hooks/useVisualMode";
 import './styles.scss'
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW"
 const Appointment = (props) => {
-  const { id, time, interview } = props
-  const renderedComponent = interview 
-    ? <Show student={interview.student} interviewer={interview.interviewer}/>
-    : <Empty />;
-  return (
-    <article className="appointment">
+  const { id, time, interview } = props;
+  const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
+  
+  let renderedComponent;
+  
+  if (mode === SHOW) {
+    renderedComponent = (
+      <Show
+        student={interview.student}
+        interviewer={interview.interviewer}
+      />);
+  }
+
+  if (mode === EMPTY) {
+    renderedComponent = <Empty />;
+  }
+    return (
+      <article className="appointment">
       <Header time={time} />
       {renderedComponent}
     </article>
