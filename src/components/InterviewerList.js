de252be7b49b454552eaf45c524afdd4535973de
William Gadd <wgadd@sasktel.net>
2022-11-08 12:33:21 -0600

    Use the PropTypes function from the prop-types library to ensure that the interviewerList component exists and is an array

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
+import PropTypes from 'prop-types';
 
 import "components/InterviewerList.scss"
 
@@ -26,5 +27,9 @@ const InterviewerList = (props) => {
   )
 
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+ };
 
 export default InterviewerList;
