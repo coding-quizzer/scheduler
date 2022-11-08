6b06469ae34f967358770416a068e46037ac3eee
William Gadd <wgadd@sasktel.net>
2022-11-08 17:46:05 -0600

    Refactor bookAppointment and cancelAppointment to calculate the daySpots from the day after the new appointment is created or deleted. Now, the count will not change when an interview is edited

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,69 +1,84 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { getDayObj } from "helpers/selectors";
+import { getDayObj, getAppointmentsForDay } from "helpers/selectors";
+
+
 export default function useApplicationData() {
   const defaultState = {
     day: 'Monday',
     days: [],
     appointments: {}
   }
-  const [state, setState] = useState(defaultState)
+  const [state, setState] = useState(defaultState);
 
-  const updateDaySpots = (state, setState, getDayObj, callback) => {
+  const getSpotsForDay = (state, day) => {
+    const appointments = (getAppointmentsForDay(state, day));
+    console.log(appointments);
+    return getAppointmentsForDay(state, day)
+    .reduce(((counter, nextAppointment) => (
+      nextAppointment.interview ? (counter): (counter + 1)
+      )), 0);
+  }
+
+  const updateDaySpots = (state, getDayObj) => {
     const newDay = {...getDayObj(state, state.day)};
-    newDay.spots = callback(newDay.spots);
+    newDay.spots = getSpotsForDay(state, state.day);
     const days = [...state.days];
     days[newDay.id - 1] = newDay;
-    setState(prev => ({...prev, days}));
+
+    return days;
   }
+
 
   const setDay = (day) => setState(prev => ({...prev, day}));
 
 
   const bookInterview = (id, interview) => {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview}
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    }
+    
     return axios({
       method: 'put',
       url: `/api/appointments/${id}`,
       data: {interview}
     })
     .then(() => {
-      setState(prev => ({
-        ...prev, 
-        appointments
-      }))
+     
+      
+      setState(prev => {
+        const appointment = {
+          ...state.appointments[id],
+          interview: { ...interview}
+        };
+        const appointments = {
+          ...prev.appointments,
+          [id]: appointment
+        };
+        const newState = {...prev, appointments};
+        const days = updateDaySpots(newState, getDayObj);
+        return {...newState, days}
+      })
     })
-    .then(() => {
-     updateDaySpots(state, setState, getDayObj, (spots) => spots-1)
-    });
   };
 
   const cancelInterview = (id) => {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    }
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    }
+    
     return axios.delete(`/api/appointments/${id}`)
     .then(() => {
-      setState(prev => ({
-        ...prev,
-        appointments
-      }))
+      
+      
+      setState(prev => {
+        const appointment = {
+          ...prev.appointments[id],
+          interview: null
+        }
+        const appointments = {
+          ...prev.appointments,
+          [id]: appointment
+        }
+        const newState = {...prev, appointments}
+        const days = updateDaySpots(newState, getDayObj);
+        return ({...newState, days});
+      })
     })
-    .then(() => {
-     updateDaySpots(state, setState, getDayObj, (spots) => spots + 1)
-    });
   };
 
   useEffect(() => {
