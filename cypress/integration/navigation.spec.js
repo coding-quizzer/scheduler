89f46301bf4c72efff76c7dc8c516b8090596187
William Gadd <wgadd@sasktel.net>
2022-11-09 15:15:42 -0600

    Change program to find tuesday by id instead of by element and ensure that the tuesday class is day-list__item-selected when Tuesday is clicked

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -8,10 +8,8 @@ describe("Navigation", () => {
   it('should navigate to Tuesday', () => {
     cy.visit("/");
     cy.get('li')
-      .contains('Tuesday')
-      .click();
-    cy.get("li")
-      .contains("li", "Tuesday")
-      .should("have.css", "background-color", "rgb(242, 242, 242)");
+      .contains('[data-testid=day]', 'Tuesday')
+      .click()
+      .should("have.class", "day-list__item--selected");
   })
 })
