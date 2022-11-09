7bcf75ed2498143c762ecd118d15c15279ff0d6a
William Gadd <wgadd@sasktel.net>
2022-11-08 19:36:57 -0600

    Add a test for an error message appearing and disappearing when a form is submitted first with no input and then with a valid input

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -47,13 +47,24 @@ describe("Form", () => {
 
   it("calls onSave function when the name and interviewer is defined", () => {
     const onSave = jest.fn();
-    const { getByText, queryByText } = render(<Form interviewers={interviewers} onSave={onSave} interviewer={1} student={"Lydia Miller-Jones"} />);
+    const { getByText, getByPlaceholderText, queryByText } = render(
+    <Form interviewers={interviewers} onSave={onSave} interviewer={1} />
+    );
+    
+    fireEvent.click(getByText("Save"));
+
+    expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
+    expect(onSave).not.toHaveBeenCalled();
+    
+    fireEvent.change(getByPlaceholderText("Enter Student Name"), { 
+      target: { value: "Lydia Miller-Jones" }
+    });
+
     fireEvent.click(getByText("Save"));
 
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
-    expect(queryByText(/pleasae select an interviewer/i)).toBeNull();
+
     expect(onSave).toHaveBeenCalledTimes(1);
     expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
   });
-
 });
