2c20ea9e790b3e4ebd2abdc3724007c59e5ed86e
William Gadd <wgadd@sasktel.net>
2022-11-09 15:53:58 -0600

    Remove extra line breaks, add semicolons for cyprus navigation tests, add steps for appointment tests to new appointment test file

---
+++ cypress/integration/appointments.spec.js
@@ -0,0 +1,38 @@
+
+describe('Appointments', () => {
+  it('should book an interview', () => {
+    // 1. Visits the root of our web server
+
+    // 2. Clicks on the "Add" button in the second appointment
+
+    // 3. Enters the name
+
+    // 4. Chooses an interviewer
+
+    // 5. Clicks the save button
+
+    // 6. Sees the booked appointment
+  });
+
+  it('should edit an interview', () => {
+    // 1. Visits the root of our web server
+
+    // 2. Clicks the edit button for the existing appointment
+
+    // 3. Changes the name and interviewer
+
+    // 4. Clicks the save button
+
+    // 5. Sees the edit to the appointment
+  });
+
+  it('should cancel an interview', () => {
+    // 1. Visits the root of our web server
+
+    // 2. Clicks the delete button for the existing appointment
+
+    // 3. Clicks the appointment button
+
+    // 4. Sees that the appointment slot is empty
+  })
+});
--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -3,7 +3,7 @@ const { CYCLIC_KEY } = require("@storybook/addon-actions");
 describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
-  })
+  });
 
   it('should navigate to Tuesday', () => {
     cy.visit("/");
@@ -11,5 +11,5 @@ describe("Navigation", () => {
       .contains('[data-testid=day]', 'Tuesday')
       .click()
       .should("have.class", "day-list__item--selected");
-  })
-})
+  });
+});
