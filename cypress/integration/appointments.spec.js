7ac9323529f1a9e289e9551e768efa0aebf71ec2
William Gadd <wgadd@sasktel.net>
2022-11-09 16:23:50 -0600

    Add cyprus test for booking an intervew

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,17 +1,31 @@
 
 describe('Appointments', () => {
   it('should book an interview', () => {
+    cy.request('GET', '/api/debug/reset');
     // 1. Visits the root of our web server
+    cy.visit('/')
+    .contains('Monday');
 
     // 2. Clicks on the "Add" button in the second appointment
+    cy.get("[alt=Add]")
+      .first()
+      .click();
 
+      
     // 3. Enters the name
+    cy.get("[placeholder='Enter Student Name']")
+    .type('Lydia Miller-Jones');
 
     // 4. Chooses an interviewer
+    cy.get("[alt='Sylvia Palmer']")
+    .click();
 
     // 5. Clicks the save button
+    cy.contains("Save").click();
 
     // 6. Sees the booked appointment
+    cy.contains('.appointment__card--show', 'Lydia Miller-Jones', 'Sylvia Palmer')
+
   });
 
   it('should edit an interview', () => {
