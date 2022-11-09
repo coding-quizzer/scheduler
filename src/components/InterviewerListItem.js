0da239ff071eccc88dee62b1bcace3e1ecbf6b31
William Gadd <wgadd@sasktel.net>
2022-11-08 21:50:06 -0600

    Add testId for interviewer Buttons

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -7,7 +7,7 @@ const InterviewerListItem = (props) => {
   const { name, avatar, selected, setInterviewer } = props;
   const itemClass = classNames('interviewers__item', {'interviewers__item--selected': selected})
   return (
-    <li className={itemClass} onClick={setInterviewer}>
+    <li className={itemClass} onClick={setInterviewer} data-testid='interviewer-item'>
       <img
         className="interviewers__item-image"
         src={avatar}
