4163f3ad8491465d35c756bdd44e6ee9b1454fd6
William Gadd <wgadd@sasktel.net>
2022-11-08 23:05:33 -0600

    Add test for canceling an interview

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByRole, getByPlaceholderText } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByRole, getByPlaceholderText, queryByText, getByAltText } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -37,5 +37,44 @@ describe("Application", () => {
     
     expect(getByText(monday, /No spots remaining/i))
   });
+
+  it("loads data, cancels an interview and increases the spots for Monday by 1", async () => {
+    // 1. Render the Application
+    const { container } = render(<Application />);
+
+    // 2. Wait until the text "Archie Cohen is displayed."
+
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    const appointment = getAllByTestId(container, 'appointment').find(appoint => queryByText(appoint, 'Archie Cohen'));
+
+    
+    
+    // 3. Click the "delete" button on the first non-empty appointment
+    fireEvent.click(getByAltText(appointment, /delete/i));
+    
+    // 4. Click the "cancel" button on the confirm menu
+    expect(getByText(appointment, /Are you sure you want to delete?/i))
+    fireEvent.click(getByText(appointment, /Cancel/i));
+    
+    // 5. Click the "delete" button again
+    
+    fireEvent.click(getByAltText(appointment, /delete/i));
+    // 6. Click the "confirm" button
+    expect(getByText(appointment, /Are you sure you want to delete?/i))
+    fireEvent.click(getByText(appointment, /Confirm/i));
+    
+    // 7. Wait until the image with the alt text 'Empty' is displayed
+    await waitForElement(() => getByAltText(appointment, 'Add'));
+
+    // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining"
+    const monday = getAllByTestId(container, 'day').find(day => getByText(day, "Monday"));
+    expect(getByText(monday, "2 spots remaining"));
+  })
+
+  it("loads data, edits an interview and keeps the spots remaining for monday the same", () => {})
+
+  it("shows the save error when failing to save an appointment", () => {})
+
+  it("shows the delete error when failing to delete an appointment", () => {})
 });
 
