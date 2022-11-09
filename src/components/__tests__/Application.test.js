40483af8b494f6e488adc95d928f691dc41214bf
William Gadd <wgadd@sasktel.net>
2022-11-08 23:33:46 -0600

    Add test for editing an interview

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByRole, getByPlaceholderText, queryByText, getByAltText } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByRole, getByPlaceholderText, queryByText, getByAltText, getByDisplayValue } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -40,7 +40,7 @@ describe("Application", () => {
 
   it("loads data, cancels an interview and increases the spots for Monday by 1", async () => {
     // 1. Render the Application
-    const { container } = render(<Application />);
+    const { container} = render(<Application />);
 
     // 2. Wait until the text "Archie Cohen is displayed."
 
@@ -62,6 +62,7 @@ describe("Application", () => {
     // 6. Click the "confirm" button
     expect(getByText(appointment, /Are you sure you want to delete?/i))
     fireEvent.click(getByText(appointment, /Confirm/i));
+    expect(getByText(appointment, "Deleting"));
     
     // 7. Wait until the image with the alt text 'Empty' is displayed
     await waitForElement(() => getByAltText(appointment, 'Add'));
@@ -71,7 +72,38 @@ describe("Application", () => {
     expect(getByText(monday, "2 spots remaining"));
   })
 
-  it("loads data, edits an interview and keeps the spots remaining for monday the same", () => {})
+  it("loads data, edits an interview and keeps the spots remaining for monday the same", async () => {
+    // 1. Render the Application
+    const { container, debug } = render(<Application />);
+
+    // 2. Wait until the text "Archie Cohen" is displayed
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    const appointment = getAllByTestId(container, 'appointment').find(appoint => queryByText(appoint, 'Archie Cohen'));
+    
+    // 3. Click the edit button on the booked appointment
+    fireEvent.click(getByAltText(appointment, 'Edit'));
+
+    // 4. Enter Willy Wonka as the student name and use Sylvia Palmer as the interviewer
+    fireEvent.change(getByDisplayValue(appointment, "Archie Cohen"), { target: { value: 'Willy Wonka' } });
+    fireEvent.click(getByAltText(appointment, 'Sylvia Palmer'));
+
+    
+    // 5. Click the Save button
+    fireEvent.click(getByText(appointment, 'Save'));
+    
+    // 6. Check that Saving is displayed on the screen
+    expect(getByText(appointment, 'Saving'));
+
+    // 7. Wait until "Willy Wonka" text is in the appointment
+    await waitForElement(() => getByText(appointment, "Willy Wonka"));
+
+    // 8. Check that the DayListItem Monday has the text "1 day remaining"
+    const monday = getAllByTestId(container, 'day').find(day => getByText(day, 'Monday'));
+    expect(getByText(monday, '1 spot remaining'));
+
+    debug();
+
+  })
 
   it("shows the save error when failing to save an appointment", () => {})
 
