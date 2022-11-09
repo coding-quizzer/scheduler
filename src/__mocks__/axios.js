2fe76ab5cbff1587fe7f59a6085be73b09f822ad
William Gadd <wgadd@sasktel.net>
2022-11-08 22:05:14 -0600

    Impliment axios mock library to support put requests

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -78,6 +78,14 @@ export default {
         data: fixtures.interviewers
       })
     }
-    
+
+  }),
+
+  put: jest.fn(url => {
+    return Promise.resolve({
+      status: 204,
+      statusText: "No Content"
+    })
+
   })
 }
