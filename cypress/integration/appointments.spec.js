0dd8fc37426e9828b1daf91a22131bf8aa166da9
William Gadd <wgadd@sasktel.net>
2022-11-09 16:53:50 -0600

    Tidy up appointment tests

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -2,60 +2,42 @@ beforeEach(() => {
   // reset database
   cy.request('GET', '/api/debug/reset');
 
-  // 1. Visits the root of our web server
   cy.visit('/')
     .contains('Monday');
 })
 describe('Appointments', () => {
   it('should book an interview', () => {
 
-    // 2. Clicks on the "Add" button in the second appointment
     cy.get("[alt=Add]")
       .first()
       .click();
 
       
-    // 3. Enters the name
-    cy.get("[placeholder='Enter Student Name']")
-    .type('Lydia Miller-Jones');
+    cy.get("[placeholder='Enter Student Name']").type('Lydia Miller-Jones');
+    cy.get("[alt='Sylvia Palmer']").click();
 
-    // 4. Chooses an interviewer
-    cy.get("[alt='Sylvia Palmer']")
-    .click();
-
-    // 5. Clicks the save button
     cy.contains("Save").click();
 
-    // 6. Sees the booked appointment
     cy.contains('.appointment__card--show', 'Lydia Miller-Jones', 'Sylvia Palmer')
 
   });
 
   it('should edit an interview', () => {
 
-    // 2. Clicks the edit button for the existing appointment
     cy.contains('.appointment__card--show', 'Archie Cohen')
     .get("[alt='Edit']")
     .click({ force: true });
 
-    // 3. Changes the name and interviewer
-    cy.get("[value='Archie Cohen']")
-      .clear()
-      .type('Lydia Miller-Jones');
+    cy.get("[value='Archie Cohen']").clear().type('Lydia Miller-Jones');
+    cy.get("[alt='Tori Malcolm'").click();
 
-    cy.get("[alt='Tori Malcolm'")
-    .click();
-
-    // 4. Clicks the save button
     cy.contains('Save')
       .click();
 
-    // 5. Sees the edit to the appointment
     cy.contains('.appointment__card--show', 'Tori Malcolm', 'Lydia Miller-Jones')
   });
 
   it('should cancel an interview', () => {
-    // 1. Visits the root of our web server
 
     // 2. Clicks the delete button for the existing appointment
 
