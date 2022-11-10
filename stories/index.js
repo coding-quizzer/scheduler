c2b116e93840a82c51dac22a1d331a3c5544f516
William Gadd <wgadd@sasktel.net>
2022-11-09 19:32:32 -0600

    Remove import actions from stories index file, since it is not used

--- stories/index.js
+++ stories/index.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
-import { action, actions } from "@storybook/addon-actions";
+import { action } from "@storybook/addon-actions";
 
 import "index.scss";
 
