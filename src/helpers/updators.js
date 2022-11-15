e8683b3426799c51142e890f843b622e10780548
William Gadd <wgadd@sasktel.net>
2022-11-15 09:08:06 -0600

    Add summaries for the helper function in updators.js

--- src/helpers/updators.js
+++ src/helpers/updators.js
@@ -1,5 +1,20 @@
 import { getDayObj, getSpotsForDay } from './selectors';
 
+/**
+ * Get a day object with the updated spots value
+ * @param {{
+ *  days,
+ *  appointments,
+ *  interviewers
+ * }} state the current state object
+ * @returns {{
+ *  id,
+ *  name,
+ *  appointments,
+ *  interviewers,
+ *  spots
+ * }} a new day object with the spots value updated
+ */
 const updateDaySpots = (state) => {
   const newDay = {...getDayObj(state, state.day)};
   newDay.spots = getSpotsForDay(state, state.day);
@@ -7,6 +22,26 @@ const updateDaySpots = (state) => {
   days[newDay.id - 1] = newDay;
   return days;
 };
+
+/**
+ * Get the updated spots object after
+ * an appointment is added, edited or deleted
+ * @param {{
+ *  days,
+ *  appointments,
+ *  interviewers
+ * }} state the previous state object
+ * @param {number} id the id of the changed appointment
+ * @param {{
+ *  student,
+ *  interviewer
+ * }} interview the newly created interview object
+ * @returns {{
+ *  days,
+ *  appointments,
+ *  interviewers
+ * }}the new state object
+ */
 
 export const updatedState = (state, id, interview) => {
 
